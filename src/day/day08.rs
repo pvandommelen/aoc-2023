c04f9325bedc8e0700d22268a6c98b416035e3f5
Peter van Dommelen <pvandommelen@users.noreply.github.com>
2023-12-10 20:21:29 +0100

    Day 8. Use an integer as a key instead of String.

--- src/day/day08.rs
+++ src/day/day08.rs
@@ -11,7 +11,21 @@ pub enum Direction {
     Right,
 }
 
-type PreparedInput = (Vec<Direction>, FxHashMap<String, (String, String)>);
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
+pub struct Node(u32);
+
+impl Node {
+    fn from_u8_slice(slice: &[u8]) -> Self {
+        assert_eq!(slice.len(), 3);
+        Node(slice.iter().fold(0u32, |num, c| (num << 8) + *c as u32))
+    }
+
+    fn ends_with(&self, c: u8) -> bool {
+        (self.0 & u8::MAX as u32) == c as u32
+    }
+}
+
+type PreparedInput = (Vec<Direction>, FxHashMap<Node, (Node, Node)>);
 
 pub fn prepare(input: &str) -> PreparedInput {
     separated_pair(
@@ -23,14 +37,14 @@ pub fn prepare(input: &str) -> PreparedInput {
         separated(
             1..,
             separated_pair(
-                alphanumeric1::<_, ()>.map(|s: &str| s.to_string()),
+                alphanumeric1::<_, ()>.map(Node::from_u8_slice),
                 " = ",
                 delimited(
                     "(",
                     separated_pair(
-                        alphanumeric1.map(|s: &str| s.to_string()),
+                        alphanumeric1.map(Node::from_u8_slice),
                         ", ",
-                        alphanumeric1.map(|s: &str| s.to_string()),
+                        alphanumeric1.map(Node::from_u8_slice),
                     ),
                     ")",
                 ),
@@ -38,25 +52,25 @@ pub fn prepare(input: &str) -> PreparedInput {
             "\n",
         ),
     )
-    .parse(input)
+    .parse(input.as_bytes())
     .unwrap()
 }
 
 pub fn solve_part1(input: &PreparedInput) -> usize {
     let (directions, map) = input;
 
-    let mut current = "AAA";
+    let mut current = Node::from_u8_slice("AAA".as_bytes());
     directions
         .iter()
         .cycle()
         .position(|direction| {
-            let (left, right) = &map[current];
+            let (left, right) = map[&current];
 
             current = match direction {
                 Direction::Left => left,
                 Direction::Right => right,
             };
-            current == "ZZZ"
+            current == Node::from_u8_slice("ZZZ".as_bytes())
         })
         .unwrap()
         + 1
@@ -66,7 +80,7 @@ pub fn solve_part2(input: &PreparedInput) -> usize {
     let (directions, map) = input;
 
     map.keys()
-        .filter(|key| key.ends_with('A'))
+        .filter(|key| key.ends_with(b'A'))
         .map(|mut current| {
             let mut encountered = FxHashMap::default();
             encountered.insert((0, current.to_owned()), 0);
@@ -82,7 +96,7 @@ pub fn solve_part2(input: &PreparedInput) -> usize {
                         Direction::Left => left,
                         Direction::Right => right,
                     };
-                    if current.ends_with('Z') {
+                    if current.ends_with(b'Z') {
                         encountered_z_distance.push(i + 1);
                     }
                     encountered

8c7c5ab924861441111f4731374c7c19af13f401
Peter van Dommelen <pvandommelen@users.noreply.github.com>
2023-12-17 13:46:40 +0100

    Day 17. Don't visit intermediate nodes.

--- src/day/day17.rs
+++ src/day/day17.rs
@@ -7,6 +7,7 @@ use bstr::ByteSlice;
 use rustc_hash::FxHashMap;
 use std::cmp::Ordering;
 use std::collections::hash_map::Entry;
+use std::ops::RangeInclusive;
 
 type PreparedInput = Grid<u8>;
 
@@ -24,17 +25,11 @@ struct State {
     target: Position,
     position: Position,
     direction: Direction,
-    direction_counter: u8,
     heat_loss: u32,
 }
 impl Ord for State {
     fn cmp(&self, other: &Self) -> Ordering {
-        let self_distance = self.position.manhattan_distance(&self.target);
-        let other_distance = other.position.manhattan_distance(&other.target);
-
-        (self.heat_loss + self_distance as u32)
-            .cmp(&(other.heat_loss + other_distance as u32))
-            .reverse()
+        self.heat_loss.cmp(&other.heat_loss).reverse()
     }
 }
 impl PartialOrd for State {
@@ -43,10 +38,7 @@ impl PartialOrd for State {
     }
 }
 
-fn solve_part<F>(grid: &PreparedInput, test_state: F) -> u32
-where
-    F: Fn(&State, &State) -> bool,
-{
+fn solve_part(grid: &PreparedInput, range: RangeInclusive<usize>) -> u32 {
     let dimensions: Dimensions = grid.dimensions.into();
 
     let mut distances = FxHashMap::with_capacity_and_hasher(grid.size(), Default::default());
@@ -58,86 +50,79 @@ where
             }
 
             let mut attempt_move = |attempt_direction: Direction| {
-                let direction_counter = if attempt_direction == state.direction {
-                    state.direction_counter + 1
-                } else {
-                    1
-                };
+                let mut position = state.position;
+                let mut next_heat_loss = state.heat_loss;
+                for steps_moved in 1..=*range.end() {
+                    match position.checked_moved(&dimensions, &attempt_direction) {
+                        None => return,
+                        Some(next) => {
+                            position = next;
+                            next_heat_loss += *grid.get(&next) as u32;
+                            if steps_moved >= *range.start() {
+                                let next_state = State {
+                                    target: state.target,
+                                    position: next,
+                                    direction: attempt_direction,
+                                    heat_loss: next_heat_loss,
+                                };
 
-                if let Some(next) = state
-                    .position
-                    .checked_moved(&dimensions, &attempt_direction)
-                {
-                    let next_heat_loss = state.heat_loss + *grid.get(&next) as u32;
+                                match distances.entry((next, attempt_direction)) {
+                                    Entry::Occupied(mut entry) => {
+                                        if *entry.get() <= next_heat_loss {
+                                            continue;
+                                        }
+                                        entry.insert(next_heat_loss);
+                                    }
+                                    Entry::Vacant(entry) => {
+                                        entry.insert(next_heat_loss);
+                                    }
+                                }
 
-                    let next_state = State {
-                        target: state.target,
-                        position: next,
-                        direction: attempt_direction,
-                        direction_counter,
-                        heat_loss: next_heat_loss,
-                    };
-
-                    if !test_state(state, &next_state) {
-                        return;
-                    }
-
-                    match distances.entry((next, attempt_direction, direction_counter)) {
-                        Entry::Occupied(mut entry) => {
-                            if *entry.get() <= next_heat_loss {
-                                return;
+                                stack.push(next_state);
                             }
-                            entry.insert(next_heat_loss.min(*entry.get()));
-                        }
-                        Entry::Vacant(entry) => {
-                            entry.insert(next_heat_loss);
                         }
                     }
-
-                    stack.push(next_state);
                 }
             };
 
-            if state.direction != Left {
+            if state.direction != Left && state.direction != Right {
                 attempt_move(Right);
             }
-            if state.direction != Up {
+            if state.direction != Up && state.direction != Down {
                 attempt_move(Down);
             }
-            if state.direction != Right {
+            if state.direction != Right && state.direction != Left {
                 attempt_move(Left);
             }
-            if state.direction != Down {
+            if state.direction != Down && state.direction != Up {
                 attempt_move(Up);
             }
             NodeResult::Next
         },
-        vec![State {
-            target: Position(dimensions.height() - 1, dimensions.width() - 1),
-            position: Position(0, 0),
-            direction: Right,
-            direction_counter: 0,
-            heat_loss: 0,
-        }],
+        vec![
+            State {
+                target: Position(dimensions.height() - 1, dimensions.width() - 1),
+                position: Position(0, 0),
+                direction: Right,
+                heat_loss: 0,
+            },
+            State {
+                target: Position(dimensions.height() - 1, dimensions.width() - 1),
+                position: Position(0, 0),
+                direction: Down,
+                heat_loss: 0,
+            },
+        ],
     )
     .heat_loss
 }
 
 pub fn solve_part1(grid: &PreparedInput) -> u32 {
-    solve_part(grid, |_, state| state.direction_counter <= 3)
+    solve_part(grid, 1..=3)
 }
 
 pub fn solve_part2(grid: &PreparedInput) -> u32 {
-    solve_part(grid, |previous_state, state| {
-        if state.position == state.target {
-            return state.direction_counter >= 4 && state.direction_counter <= 10;
-        }
-        if previous_state.direction == state.direction {
-            state.direction_counter <= 10
-        } else {
-            previous_state.direction_counter >= 4
-        }
-    })
+    solve_part(grid, 4..=10)
 }
 
 pub fn solve(input: &str) -> (Solution, Solution) {
